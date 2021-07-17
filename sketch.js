d1ab7ce17e945b4a0fc42084f6354a978f18beb5
Cesar1357 <85760661+Cesar1357@users.noreply.github.com>
2021-07-17 15:31:32 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,17 +74,17 @@ function draw() {
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
 
-	if(keyCode === LEFT_ARROW){
-		helicopterSprite.x=helicopterSprite.x -20;
-		Matter.Body.translate(packageBody, {x:-20,y:0})
+	if(keyDown("LEFT_ARROW")){
+		helicopterSprite.x=helicopterSprite.x -8
+		Matter.Body.translate(packageBody, {x:-8,y:0})
 	}  
 
-	if(keyCode === RIGHT_ARROW){
-		helicopterSprite.x=helicopterSprite.x +20;
-		Matter.Body.translate(packageBody, {x:20,y:0})
+	if(keyDown("RIGHT_ARROW")){
+		helicopterSprite.x=helicopterSprite.x +8;
+		Matter.Body.translate(packageBody, {x:8,y:0})
 	} 
 	
-	if(keyCode === DOWN_ARROW){
+	if(keyDown("DOWN_ARROW")W){
 		Matter.Body.setStatic(packageBody,false);
 	}
 	
